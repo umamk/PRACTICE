97bcc94cb81be3f5dff60997e855703cdfb09464
umamk <umaitr123@gmail.com>
2021-05-11 20:17:27 -0500

    practice

---
+++ src/com/test/Multiplication.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int c ;
+		
+		for(int b = 1;b<=10; b++) 
+		{
+			c = a*b;
+			System.out.println(a+"*"+b+"="+c);
+	}
+		}
+
+}
